4ce007ff897a49bb245645113d61eb90fd735b0a
Santhosh Kumar <84244864+25122001@users.noreply.github.com>
2023-06-10 16:32:42 +0530

    50 videos completed

--- Javascript learn/Secondjs.js
+++ Javascript learn/Secondjs.js
@@ -40,6 +40,7 @@ console.log("output:", cartobj);
 
 const user = {
     name:'anbu',
+    age: 24,
     getfullname()
     {
         console.log(`my name is ${this.name}`);
@@ -63,3 +64,86 @@ if ('name' in user)
 {
     console.log("yes");
 }
+
+///////////////////////////////Cloning of an object///////////////////////////////////////////////
+
+let another ={};
+for(let key in user){
+    another[key] = user[key];//another['name'] = user['name']
+console.log(key);
+
+}
+
+///////////////////////////////     OR    ///////////////////////////////////////////////
+
+let another1 ={...user};  ///by using spread operator
+console.log(another1);
+
+////////////////////////   Finding Maximum marks   ///////////////////////////
+
+
+let marks = [45,74,5,35];
+let result = Math.max(...marks);
+console.log("Maximum marks:" + result);
+
+////////////////////////   object and string identification   ///////////////////////////
+
+let firstname = " Santhosh..";
+let lastname = "kumar";  //typeof lastname it is in string
+const firstname1 = new  String("santhosh");  // typeof firstname1 it is in object
+
+////////////////////////////////////   string functions  ///////////////////////////////////
+
+console.log(firstname.concat(' ',lastname));
+console.log(firstname.includes("oh"));
+console.log(firstname.startsWith("Sa"));
+console.log(firstname.endsWith("ar"));
+console.log(firstname.repeat(10));
+console.log(lastname.replace("kumar","Rocky"));
+console.log(firstname.slice(3));
+let para = "some text";
+console.log(para.split(' ', 1));
+console.log(firstname.toUpperCase());
+console.log(firstname.trim());
+console.log(firstname.trimStart());
+
+////////////////////////////////////   Template Literals or (backticks) ///////////////////////////////////
+const info = "thank you \n for watching"; ///escape sequences
+const info1 = "thank you \t for watching";
+console.log(info);
+const template = `without using "escape 
+sequences" it is so helpful`;
+console.log(template);
+
+////////////////////////////////////  Date object ///////////////////////////////////
+ 
+const cal = Date().toString();// it is in string now
+const now = new Date();   // it is in object
+console.log(cal);
+console.log(now.getFullYear());
+const anotherDate = new Date("July 6 1996");
+console.log(anotherDate.getFullYear());
+
+let anotherDate2 = new Date();
+anotherDate2.setFullYear("2000");
+console.log(anotherDate2.getFullYear()); //it is in object
+
+//////   how date is taken different countries
+
+let [month, date, year] = new Date().toLocaleDateString("en-IN").split("/");
+console.log(month, date, year);
+
+
+/////////----------------------->UPTO THIS OBJECT of oops COMPLETED
+
+////////////////////////////////////  ARRAYS of oops ///////////////////////////////////
+
+// add ,filter, remove, split, combine  of element in  arrays
+
+const number = ["Apple", "Lemon", "Grapes"]  ///here we used const variable the value can be updated because it is array
+number[3] = "orange"  // push // unshift  // splice is introduced without declaring array location
+number.push("strawberry") //puts in last
+number.unshift("guava") // puts in first
+number.splice(3, 0, "watermelons")//insert location(3),delete location (0 = nothing)
+console.log(number[1])
+console.log(number);
